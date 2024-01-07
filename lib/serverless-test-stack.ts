717a7e64f9e44895c38089bdcfa895c348f8cad4
akalanka <akalanka@ensiz.com>
2024-01-07 14:55:22 +0530

    grant write permissions to lambda for s3 and dynamo

--- lib/serverless-test-stack.ts
+++ lib/serverless-test-stack.ts
@@ -46,6 +46,9 @@ export class ServerlessTestStack extends cdk.Stack {
       },
     });
 
+    messagesTable.grantWriteData(messagesPostLambda);
+    messagesBucket.grantWrite(messagesPostLambda);
+
     // API Gateway & log group
     const logGroup = new LogGroup(this, "myCarbonRestAPIAccessLogs", {
       retention: RetentionDays.ONE_DAY,
