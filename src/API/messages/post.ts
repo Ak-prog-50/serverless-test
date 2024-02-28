09eeaf79ba9fbc64509a6ee0c94fb6102f810032
akalanka <akalankapathirage22@gmail.com>
2024-02-28 21:20:15 +0530

    installed sqs sdk + refactored lambda code + sending responses to a queue

--- src/API/messages/post.ts
+++ src/API/messages/post.ts
@@ -1,17 +1,18 @@
 import { SQSHandler } from "aws-lambda";
-import { ApiResponse } from "../../utils/ApiResponse";
 import { ApiError } from "../../utils/ApiError";
 import { env } from "process";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { SendMessageBatchCommand, SQSClient } from "@aws-sdk/client-sqs";
+import { ApiResponse } from "../../utils/ApiResponse";
 
-const { MESSAGES_BUCKET_NAME, MESSAGES_TABLE_NAME } = env;
+const { MESSAGES_BUCKET_NAME, MESSAGES_TABLE_NAME, RESPONSE_QUEUE_URL } = env;
 const s3Client = new S3Client({});
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
+const sqsClient = new SQSClient({});
 
-// Request event's body should be a JSON stringified object of this type
 type TRequestBody = {
   message: {
     metadata: {
@@ -27,54 +28,85 @@ type TRequestBody = {
   };
 };
 
-export const handler: SQSHandler = async (event) => {
-  try {
-    if (!MESSAGES_BUCKET_NAME || !MESSAGES_TABLE_NAME)
-      throw ApiError.internal("ENV variables are not set!");
-    const requestBody: TRequestBody = JSON.parse(event.Records[0].body);
-    console.log("request body", requestBody); //  could records[0] be a problem since batch size is 10?
-    if (!requestBody || Object.keys(requestBody).length !== 1)
-      throw ApiError.badRequest("Invalid request body!");
+const validateEnvVariables = () => {
+  if (!MESSAGES_BUCKET_NAME || !MESSAGES_TABLE_NAME || !RESPONSE_QUEUE_URL)
+    throw ApiError.internal("Environment variables are not set!");
+};
 
-    const message = requestBody.message;
+const validateMessage = (message: any) => {
+  const expectedKeys = ["message_time", "company_id", "message_id"] as const;
+  return (
+    message.metadata &&
+    expectedKeys.every(
+      (key) => key in message.metadata && message.metadata[key]
+    )
+  );
+};
 
-    // message validation logic - all field in metadata are required.
-    const expectedKeys = ["message_time", "company_id", "message_id"] as const;
-    const isMessageValid =
-      message.metadata &&
-      expectedKeys.every(
-        (key) => key in message.metadata && message.metadata[key]
-      );
-    if (!isMessageValid) throw ApiError.badRequest("Message is Invalid!");
+const persistMessage = async (message: TRequestBody["message"]) => {
+  const { message_id, company_id } = message.metadata;
 
-    const { message_id, company_id } = message.metadata;
+  await s3Client.send(
+    new PutObjectCommand({
+      Bucket: MESSAGES_BUCKET_NAME,
+      Key: `${company_id}/${message_id}.json`,
+      Body: JSON.stringify(message),
+    })
+  );
 
-    await s3Client.send(
-      new PutObjectCommand({
-        Bucket: MESSAGES_BUCKET_NAME,
-        Key: `${company_id}/${message_id}.json`,
-        Body: JSON.stringify(message),
-      })
-    );
-
-    const command = new PutCommand({
+  await docClient.send(
+    new PutCommand({
       TableName: MESSAGES_TABLE_NAME,
       Item: {
         messageId: message_id,
         message: JSON.stringify(message),
       },
-    });
+    })
+  );
+};
 
-    await docClient.send(command);
+const sendMessageToSQS = async (queueUrl: string, body: ApiResponse | ApiError) => {
+  await sqsClient.send(
+    new SendMessageBatchCommand({
+      QueueUrl: queueUrl,
+      Entries: [
+        {
+          Id: Date.now().toString(), // Unique id for the message, consider a more robust unique identifier
+          MessageBody: JSON.stringify(body),
+        },
+      ],
+    })
+  );
+};
 
-    const apiResponse = ApiResponse.success("Messages procced and stored!");
-    // TODO: Send the success message into a sqs queue
-  } catch (error) {
-    // Rethrows error if it's unknwon type. Lambda will get notified of the invocation failure and resend the message to queue.
-    console.error(error);
-    if (error instanceof ApiError) {
-      // TODO: send the error message into a sqs queue
+export const handler: SQSHandler = async (event) => {
+  validateEnvVariables();
+
+  for (const record of event.Records) {
+    try {
+      console.log("Processing record", record.messageId);
+      const requestBody: TRequestBody = JSON.parse(record.body);
+      if (!validateMessage(requestBody.message)) {
+        throw ApiError.badRequest("Invalid message format");
+      }
+
+      await persistMessage(requestBody.message);
+
+      await sendMessageToSQS(
+        RESPONSE_QUEUE_URL as string,
+        ApiResponse.success(`Message processed and stored: ${record.messageId}`)
+      );
+    } catch (error: any) {
+      console.error("Error processing: ", record.messageId, error);
+      await sendMessageToSQS(
+        RESPONSE_QUEUE_URL as string,
+        ApiError.internal(
+          `Error processing message: ${record.messageId}, Error: ${error.message}`
+        )
+      );
+      if (!(error instanceof ApiError)) {
+        throw error; // Rethrow error for AWS Lambda to handle the retry
+      }
     }
-    else throw error;
   }
 };
